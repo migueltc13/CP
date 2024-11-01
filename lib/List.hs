9c812b13388e25144d8d79648c8c5338271d70b3
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-11-01 20:46:09 +0000

    Update List.hs

--- lib/List.hs
+++ lib/List.hs
@@ -1,8 +1,8 @@
-{-# OPTIONS_GHC -XNPlusKPatterns #-}
+{-# LANGUAGE NPlusKPatterns #-}
 
--- (c) MP-I (1998/9-2006/7) and CP (2005/6-2023/24)
+-- (c) MP-I (1998/9-2006/7) and CP (2005/6-2024/25)
 
-module List  where
+module List where
 
 import Cp
 import Data.List
@@ -16,7 +16,7 @@ import Nat hiding (fac)
 inList = either nil cons
 
 outList []    = i1 ()
-outList (a:x) = i2(a,x)
+outList (a:x) = i2 (a,x)
 
 baseList g f = id -|- g >< f
 
@@ -24,7 +24,7 @@ baseList g f = id -|- g >< f
 
 recList  f   = id -|- id >< f                   -- this is F f for this data type
 
-cataList g   = g . recList (cataList g) . outList   
+cataList g   = g . recList (cataList g) . outList
 
 anaList  g   = inList . recList (anaList g) . g
 
@@ -38,14 +38,14 @@ hyloList f g = cataList f . anaList g
 
 -- (4.1) number representation (base b) evaluator ------------------------------
 
-eval b = cataList (either zero (add.(id><(b*))))
+eval b = cataList (either zero (add . (id >< (b*))))
 
 -- eval b [] = 0
 -- eval b (x:xs) = x + b * (eval b xs)
 
 -- (4.2) inversion -------------------------------------------------------------
 
-reverse' = cataList (either nil snoc) where snoc(a,l) = l ++ [a]
+reverse' = cataList (either nil snoc) where snoc (a,l) = l ++ [a]
 
 -- alternatively: snoc = conc . swap . (singl >< id)
 --                       where singl a = [a]
@@ -65,17 +65,17 @@ lookup k = cataList (either nothing aux)
 iSort :: Ord a => [a] -> [a]
 iSort =  cataList (either nil insert)
          where insert(x,[])              = [x]
-               insert(x,a:l) | x < a     = [x,a]++l
-                             | otherwise = a:(insert(x,l))
+               insert(x,a:l) | x < a     = [x,a] ++ l
+                             | otherwise = a : insert (x,l)
 
 -- also iSort = hyloList (either (const []) insert) outList
 
 -- (4.5) take (cf GHC.List.take) -----------------------------------------------
 
 utake = anaList aux
-         where aux(0,_) = i1()
-               aux(_,[]) = i1()
-               aux(n,x:xs) = i2(x,(n-1,xs))
+         where aux (0,_) = i1 ()
+               aux (_,[]) = i1 ()
+               aux (n,x:xs) = i2 (x,(n-1,xs))
 
 -- pointwise version:
 -- take 0 _ = []
@@ -87,12 +87,12 @@ utake = anaList aux
 fac :: Integer -> Integer
 fac = hyloList (either (const 1) mul) natg
 
-natg = (id -|- (split succ id)) . outNat
+natg = (id -|- split succ id) . outNat
 
 -- (4.6.1) Factorial (alternative) ---------------------------------------------
 
 fac' = hyloList (either (const 1) (mul . (succ >< id)))
-                 ((id -|- (split id id)) . outNat)
+                        ((id -|- split id id) . outNat)
 
 {-- cf:
 
@@ -112,9 +112,9 @@ evens = anaList evend
 
 odds  = anaList oddd
 
-evend = (id -|- (split (2*) id)) . outNat
+evend = (id -|- split (2*) id) . outNat
 
-oddd  = (id -|- (split odd id)) . outNat
+oddd  = (id -|- split odd id) . outNat
        where odd n = 2*n+1
 
 {-- pointwise:
@@ -124,61 +124,63 @@ sq (n+1) = 2*n+1 + sq n
 cf. Newton's binomial: (n+1)^2 = n^2 + 2n + 1
 --}
 
--- (4.7.1) Square function reusing anaList of factorial ----------------------------
+-- (4.7.1) Square function reusing anaList of factorial ------------------------
 
-sq' = (cataList summing) . fmap (\n->2*n-1) . (anaList natg)
+sq' = cataList summing . fmap (\n->2*n-1) . anaList natg
 
 -- (4.8) Prefixes and suffixes -------------------------------------------------
 
 prefixes :: Eq a => [a] -> [[a]]
 prefixes = cataList (either (const [[]]) scan)
-           where scan(a,l) = [[]] ++ (map (a:) l)
+           where scan(a,l) = [] : map (a:) l
 
 suffixes = anaList g
-           where g = (id -|- (split cons p2)) . outList
+           where g = (id -|- split cons p2) . outList
 
 diff :: Eq a => [a] -> [a] -> [a]
 diff x l = cataList (either nil (g l)) x
-           where g l (a,x) = if (a `elem` l) then x else (a:x)
+           where g l (a,x) = if a `elem` l then x else a : x
 
 -- (4.9) Grouping --------------------------------------------------------------
 
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf n = anaList (g n) where
-          g n [] = i1()
-          g n x = i2(take n x,drop n x)
+          g n [] = i1 ()
+          g n x = i2 (splitAt n x)
+          -- g n x = i2 (take n x,drop n x)
 
 nest = chunksOf
 
--- (4.10) Relationship with foldr, foldl ----------------------------------------
+-- (4.10) Relationship with foldr, foldl ---------------------------------------
 
 myfoldr :: (a -> b -> b) -> b -> [a] -> b
 myfoldr f u = cataList (either (const u) (uncurry f))
 
 myfoldl :: (a -> b -> a) -> a -> [b] -> a
 myfoldl f u = cataList' (either (const u) (uncurry f . swap))
-              where cataList' g   = g . recList (cataList' g) . outList'   
-                    outList' [] = i1()
-                    outList' x =i2(last x, blast x)
+              where cataList' g = g . recList (cataList' g) . outList'
+                    outList' [] = i1 ()
+                    outList' x = i2 (last x, blast x)
                     blast = tail . reverse
 
--- (4.11) No repeats ------------------------------------------------------------
+-- (4.11) No repeats -----------------------------------------------------------
 
 nr :: Eq a => [a] -> Bool
 nr = p2 . aux where
      aux = cataList (either f (split g h))
      f _ = ([],True)
-     g(a,(t,b)) = a:t
-     h(a,(t,b)) = not(a `elem` t) && b
+     g (a,(t,b)) = a:t
+     h (a,(t,b)) = notElem a t && b
+     -- h(a,(t,b)) = not (a `elem` t) && b
 
--- (4.12) Advanced --------------------------------------------------------------
+-- (4.12) Advanced -------------------------------------------------------------
 
--- (++) as a list catamorphism ------------------------------------------------
+-- (++) as a list catamorphism -------------------------------------------------
 
 ccat :: [a] -> [a] -> [a]
 ccat = cataList (either (const id) compose). map (:) where
-       -- compose(f,g) = f.g
-       compose =  curry(ap.(id><ap).assocr)
+       -- compose(f,g) = f . g
+       compose =  curry (ap . (id >< ap) . assocr)
 
 -- monadic map
 -- mmap f = cataList $ either (return.nil)(fmap cons.dstr.(f><id))
@@ -187,25 +189,25 @@ mmap f (h:t) = do { b <- f h ; x <- mmap f t ; return (b:x) }
 
 -- distributive law
 lam  :: Strong m => [m a] -> m [a]
-lam = cataList ( either (return.nil)(fmap cons.dstr) )
+lam = cataList ( either (return.nil) (fmap cons.dstr) )
 
 -- monadic catas
 
 mcataList :: Strong ff => (Either () (b, c) -> ff c) -> [b] -> ff c
-mcataList g = g .! (dl . recList (mcataList g) . outList)   
+mcataList g = g .! (dl . recList (mcataList g) . outList)
 
 dl :: Strong m => Either () (b, m a) -> m (Either () (b, a))
-dl = either (return.i1)(fmap i2. lstr)
+dl = either (return.i1) (fmap i2. lstr)
 
---lam' = mcataList (either (return.nil)(fmap cons.rstr))
+-- lam' = mcataList (either (return.nil)(fmap cons.rstr))
 
 -- streaming -------------------------------------------------------------------
 
 stream f g c x = case f c of
-   Just (b, c') -> b : stream f g c' x
-   Nothing      ->  case x of
-                      a:x' -> stream f g (g c a) x'
-                      []   -> []
+  Just (b, c') -> b : stream f g c' x
+  Nothing      ->  case x of
+                     a:x' -> stream f g (g c a) x'
+                     []   -> []
 
 -- heterogeneous lists ---------------------------------------------------------
 
@@ -213,6 +215,7 @@ join :: ([a], [b]) -> [Either a b]
 join (a, b) = map i1 a ++ map i2 b
 
 sep = split s1 s2 where
-   s1 []=[]; s1(Left a:x) = a:s1 x; s1(Right b:x)=s1 x
-   s2 []=[]; s2(Left a:x) = s2 x; s2(Right b:x)=b:s2 x
+   s1 [] = []; s1 (Left a:x) = a : s1 x; s1(Right b:x) = s1 x
+   s2 [] = []; s2 (Left a:x) = s2 x;     s2(Right b:x) = b : s2 x
+
 ---- end of List.hs ------------------------------------------------------------
